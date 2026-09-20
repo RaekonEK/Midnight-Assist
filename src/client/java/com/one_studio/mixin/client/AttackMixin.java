9501f9c7f72feb0878f5d4ce19ea2550ddbf1569
1studios <raekonelking@gmail.com>
2026-09-20 14:19:26 +0100

    Add files via upload

---
+++ src/client/java/com/one_studio/mixin/client/AttackMixin.java
@@ -0,0 +1,18 @@
+package com.one_studio.mixin.client;
+
+import com.one_studio.MidnightAssisitClient;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public class AttackMixin {
+    @Inject(at = @At("HEAD"), method = "doAttack", cancellable = true)
+    private void onDoAttack(CallbackInfoReturnable<Boolean> cir) {
+        if (MidnightAssisitClient.tryInterceptAttack((MinecraftClient)(Object)this)) {
+            cir.setReturnValue(false);
+        }
+    }
+}
---
+++ src/client/java/com/one_studio/mixin/client/CameraMixin.java
@@ -0,0 +1,18 @@
+package com.one_studio.mixin.client;
+
+import com.one_studio.MidnightAssisitClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class CameraMixin {
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
+        float partialTicks = tickCounter.getTickProgress(tick);
+        MidnightAssisitClient.applyPartialTicks(partialTicks);
+    }
+}
